e1d15530b558b36968335a9298a844dd82756bdf
JackieLin <136216354+JackieLin2004@users.noreply.github.com>
2024-03-17 19:35:58 +0800

    Add files via upload

---
+++ WeChat/js/myJS.js
@@ -0,0 +1,15 @@
+function TurnToIndex() {
+    window.location.href = 'index.html';
+}
+
+function TurnToContacts() {
+    window.location.href = 'contacts.html';
+}
+
+function TurnToFound() {
+    window.location.href = 'found.html';
+}
+
+function TurnToMe() {
+    window.location.href = 'me.html';
+}
